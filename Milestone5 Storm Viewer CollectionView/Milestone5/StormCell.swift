f25d405e1ac411252d70dff24a319f283e6585ad
Mike Killewald <evdev2000@yahoo.com>
2017-07-30 11:51:12 -0400

    completed Milestone 5

---
+++ Milestone5 Storm Viewer CollectionView/Milestone5/StormCell.swift
@@ -0,0 +1,14 @@
+//
+//  StormCell.swift
+//  Milestone5
+//
+//  Created by Mike Killewald on 7/30/17.
+//  Copyright © 2017 Gameaholix. All rights reserved.
+//
+
+import UIKit
+
+class StormCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+}
---
+++ Milestone5 Storm Viewer CollectionView/Milestone5/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  Milestone5
+//
+//  Created by Mike Killewald on 7/12/17.
+//  Copyright © 2017 Gameaholix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        title = "Storm Viewer"
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Storm", for: indexPath) as! StormCell
+
+        cell.name.text = pictures[indexPath.row]
+        
+        cell.imageView.image = UIImage(named: pictures[indexPath.row])
+        cell.imageView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            // 2: success! Set its selectedImage property
+            vc.selectedImage = pictures[indexPath.row]
+            
+            // 3: now push it onto the navigation controller
+            navigationController?.pushViewController(vc, animated: true)
+        }
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
